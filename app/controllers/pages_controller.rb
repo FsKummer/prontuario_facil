4b6c689bf9201c7559dbf05424dbba01147a835c
Felipe Kummer <felipe.s.kummer@gmail.com>
2025-01-03 16:33:20 -0300

    feat: create dashboard front-end with turbo

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,23 @@
 class PagesController < ApplicationController
+  before_action :set_user, only: :home
+
   def home
-    if user_signed_in?
-      render "user_home"
-    else
-      render "home"
+    @user ? render_user_dashboard : render("home")
+  end
+
+  private
+
+  def render_user_dashboard
+    respond_to do |format|
+      if turbo_frame_request? && turbo_frame_request_id == "content"
+        format.html { render partial: "pages/partials/dash_#{params[:partial]}" }
+      else
+        format.html { render "user_home" }
+      end
     end
+  end
+
+  def set_user
+    @user = current_user if user_signed_in?
   end
 end
